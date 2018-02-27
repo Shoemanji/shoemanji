ebc9301ce24851cb71af63b2e05255b6dd216e61
Alfonso Millan <alfonso.a.millan@gmail.com>
2018-02-27 11:24:23 -0500

    Products table #3

--- server/db/models/products.js
+++ server/db/models/products.js
@@ -1,1 +1,38 @@
-console.log('testing new branch');
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+const Products = db.define('products', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.TEXT,
+  },
+  price: {
+    type: Sequelize.DECIMAL,
+    allowNull: false,
+    validate: {
+      min: 0.00,
+    }
+  },
+  inventory: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0,
+    validate: {
+      min: 0,
+    }
+  },
+  category: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  }
+}, {
+
+});
+
+module.exports = Products;
+
+// TODO: write class method maybe for inventory subtraction
+
+// TODO: talk about possible instance  methods with group
---
+++ server/db/models/products.specs.js
@@ -0,0 +1,29 @@
+/* global describe beforeEach it */
+
+const {expect} = require('chai')
+const db = require('../index')
+const User = db.model('user')
+
+// TODO: come up with test specs here
+
+describe('Products model', () => {
+  beforeEach(() => {
+    return db.sync({force: true})
+  })
+
+  describe('instanceMethods', () => {
+    describe('todo', () => {
+
+      beforeEach(() => {
+        return Products.create({
+          todo1: 'xxx',
+          todo2: 'xxx'
+        })
+          .then(user => {
+            cody = user
+          })
+      })
+
+    })
+  })
+})
