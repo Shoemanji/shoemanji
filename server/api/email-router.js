79effb6a319a65a2fb09bad3eb325f25175b3e03
Alfonso Millan <alfonso.a.millan@gmail.com>
2018-03-05 18:20:04 -0500

    feature: email after checkout

---
+++ server/api/email-router.js
@@ -0,0 +1,55 @@
+const emailRouter = require('express').Router();
+const nodemailer = require('nodemailer');
+const Mailgen = require('mailgen');
+
+emailRouter.post('/', (req, res, next) => {
+  const mailGenerator = new Mailgen({
+    theme: 'default',
+    product: {
+      name: 'Shoemanji',
+      link: 'http://localhost:8080/',
+    }
+  });
+
+  const createdOrderEmail = {
+    body: {
+        name: req.user.dataValues.email,
+        intro: 'Thank you for your recent purchase!',
+        action: {
+            instructions: 'To review your order, please click here:',
+            button: {
+                color: '#1E90FF', // Optional action button color
+                text: 'Check your order',
+                link: `http://localhost:8080/user/${req.user.dataValues.id}/orders`
+            }
+        },
+        outro: 'Need help, or have questions? Just reply to this email, we\'d love to help.'
+    }
+  };
+
+  // Generate an HTML email with the provided contents
+  const emailBody = mailGenerator.generate(createdOrderEmail);
+
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_FROM,
+      pass: process.env.EMAIL_PW,
+    }
+  });
+
+  let mailOptions = {
+    from: process.env.EMAIL_FROM,
+    to: req.user.dataValues.email,
+    subject: 'Order Confirmation',
+    text: 'test',
+    html: emailBody,
+  };
+
+  transporter.sendMail(mailOptions, (err, info) => {
+    if (err) res.sendStatus(401).json({ err: info });
+    res.sendStatus(200).json({ success: true });
+  })
+});
+
+module.exports = emailRouter;
--- server/api/index.js
+++ server/api/index.js
@@ -4,11 +4,13 @@ const userRouter = require('./users');
 const productRouter = require('./products');
 const reviewRouter = require('./reviews');
 const orderRouter = require('./orders');
+const emailRouter = require('./email-router');
 
 router.use('/users', userRouter);
 router.use('/products', productRouter);
-router.use('/reviews', reviewRouter)
-router.use('/orders', orderRouter)
+router.use('/reviews', reviewRouter);
+router.use('/orders', orderRouter);
+router.use('/sendmail', emailRouter);
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
